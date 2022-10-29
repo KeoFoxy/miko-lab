e7bee287c8911c4ca0028b7e2a0bf375df766794
KeoFoxy <arasdiel68@gmail.com>
2022-10-29 22:38:00 +0300

    Banner completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import { NavBar } from './components/NavBar.js'
+import { Banner } from './components/Banner.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Banner />
     </div>
   );
 }
