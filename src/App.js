77983d2659062683e301966477b7e05b1c60e7a2
KeoFoxy <arasdiel68@gmail.com>
2022-10-29 20:09:17 +0300

    Bootstrap react library has been added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
         <h1>Hello World</h1>
+        <Button variant="primary">bootstrap works</Button>
     </div>
   );
 }
