827e8b9251e4e8b86e145f3d6973f2aeee27822a
KeoFoxy <arasdiel68@gmail.com>
2022-10-30 20:09:14 +0300

    Contact component added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Banner } from './components/Banner.js'
 import { Skills } from './components/Skills'
 import { Projects} from './components/Projects'
 import { Footer } from './components/Footer';
+import { Contact } from './components/Contact'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
       <Footer />
     </div>
   );
