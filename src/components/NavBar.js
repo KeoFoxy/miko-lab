96f4861780ac6496e838d42aebe1f7de57f66b08
KeoFoxy <arasdiel68@gmail.com>
2022-10-30 20:09:38 +0300

    hashlink to connect button added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,65 +1,66 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import { HashLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+export const NavBar = () => {
 
-import logo from '../assets/img/logo.svg'
-import navIcon1 from '../assets/img/nav-icon1.svg'
-import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
-//import NavDropdown from 'react-bootstrap/NavDropdown';
-
-export const NavBar = () =>{
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
 
-  useEffect(()=>{
-    const onScroll = () =>{
-      if(window.screenY > 50){
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
         setScrolled(true);
       } else {
         setScrolled(false);
       }
     }
 
-
     window.addEventListener("scroll", onScroll);
 
-    return () => window.removeEventListener("scroll", onScroll)
+    return () => window.removeEventListener("scroll", onScroll);
   }, [])
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
   }
 
-
-    return(
-        <Navbar /*bg="light"*/ expand="lg" className={scrolled ? "scrolled" : ""}>
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="/">
             <img src={logo} alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className='navbar-toggler-icon'></span>
+            <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             </Nav>
-            <span className='navbar-text'>
-              <div className='social-icon'>
-                <a href="#"><img src={navIcon1} alt=""/></a> 
-                <a href="#"><img src={navIcon2} alt=""/></a>
-                <a href="#"><img src={navIcon3} alt=""/></a>
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
-              <button className='vvd' onClick={()=> console.log('connect')}><span>Contact us</span></button>
+              <HashLink to='#connect'>
+                <button className="vvd"><span>Contact us</span></button>
+              </HashLink>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    )
+    </Router>
+  )
 }
-
