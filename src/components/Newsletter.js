4eb86274384b1eaaaf82ba7335e74412d18de9c5
KeoFoxy <arasdiel68@gmail.com>
2022-10-30 18:55:19 +0300

    Localhost changed

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,33 +1,32 @@
-import { Container, Row, Col } from "react-bootstrap"
-import { useState, useEffect } from "react"
-import e from "express"
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
 
-export const Newsletter = ({ onValidated, status, message }) => {
-    const [email, setEmail] = useState('')
+export const Newsletter = ({ status, message, onValidated }) => {
+    const [email, setEmail] = useState('');
 
     useEffect(() => {
-        if(status === 'success') clearFields()
+        if (status === 'success') clearFields();
     }, [status])
 
-    const handleSubmit= () =>{
-        e.preventDefault()
+    const handleSubmit = (e) => {
+        e.preventDefault();
         email &&
-        email.indexOf("@") > -1 &&
-        onValidated({
-            EMAIL: email
-        })
+            email.indexOf("@") > -1 &&
+            onValidated({
+                EMAIL: email
+            })
     }
 
-    const clearFields = () =>{
-        setEmail('')
+    const clearFields = () => {
+        setEmail('');
     }
 
-    return(
+    return (
         <Col lg={12}>
-            <div className="newsletter-bx">
+            <div className="newsletter-bx wow slideInUp">
                 <Row>
                     <Col lg={12} md={6} xl={5}>
-                        <h3>Subscribe to our Newsletter</h3>
+                        <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
                         {status === 'sending' && <Alert>Sending...</Alert>}
                         {status === 'error' && <Alert variant="danger">{message}</Alert>}
                         {status === 'success' && <Alert variant="success">{message}</Alert>}
@@ -35,7 +34,7 @@ export const Newsletter = ({ onValidated, status, message }) => {
                     <Col md={6} xl={7}>
                         <form onSubmit={handleSubmit}>
                             <div className="new-email-bx">
-                                <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address"/>
+                                <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
                                 <button type="submit">Submit</button>
                             </div>
                         </form>
